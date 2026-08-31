ca56424f736ad0f8a3ef776ff67dd60f703104fa
Anton Malygin <ai.malygin@icloud.com>
2026-08-31 10:50:53 -0500

    release: prepare mobile v0.4.1-rc.4

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 import Foundation
 import PackageDescription
 
-let releaseVersion = "0.4.1-rc.3"
-let releaseChecksum = "ac1d91e1fe553d21e5a3c030e6096a66032a6b5ef516411c1ad7edd8b5c04ed9"
+let releaseVersion = "0.4.1-rc.4"
+let releaseChecksum = "0000000000000000000000000000000000000000000000000000000000000000"
 let localXCFrameworkPath = "Artifacts/XrayRust.xcframework"
 let packageDirectory = URL(fileURLWithPath: #filePath).deletingLastPathComponent()
 let localXCFrameworkURL = packageDirectory.appendingPathComponent(
